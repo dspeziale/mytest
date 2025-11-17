c8384d0244391720c90c97764469439ba269d40d
Daniele.Speziale <dspeziale@gmail.com>
2025-11-17 11:55:44 +0100

    17/11

--- tests/test_main.py
+++
@@ -1,9 +0,0 @@
-from src.myproject.main import hello
-
-
-def test_hello_default():
-    assert hello("Mondo") == "Ciao, Mondo!"
-
-
-def test_hello_name():
-    assert hello("Daniele") == "Ciao, Daniele!"
